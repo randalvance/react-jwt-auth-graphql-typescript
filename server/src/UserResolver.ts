76be027b684657a48d4e7f98859d2fe3cdb72779
Randal Vance Cunanan <randal@randalvance.net>
2020-03-01 19:36:07 +0800

    Added login endpoint

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,14 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from 'type-graphql';
+
 import { User } from './entity/User';
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -12,6 +20,34 @@ export class UserResolver {
     @Query(() => [User])
     users() {
         return User.find();
+    }
+
+    
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg('email') email: string,
+        @Arg('password') password: string,
+    ): Promise<LoginResponse> {
+        const user = await User.findOne({ where: { email }});
+
+        if (!user) {
+            throw new Error("User was not found!");
+        }
+
+        const valid = await compare(password, user.password);
+
+        if (!valid) {
+            throw new Error("Invalid password!");
+        }
+
+        // Login Successfull
+
+        return {
+            accessToken: sign(
+                { userId: user.id },
+                "secret",
+                { expiresIn: "15m" })
+        };
     }
 
     @Mutation(() => Boolean)
