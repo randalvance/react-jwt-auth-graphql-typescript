f669c7f59b852d62f97f4c921f94b917e5af078b
Randal Vance Cunanan <randal@randalvance.net>
2020-03-02 22:47:03 +0800

    Add Register Form

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
 
 }
 
 export const Register: React.FC<Props> = () => {
-    return <div>Register Page</div>;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    return (
+        <>
+            <h1>Register</h1>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                console.log('Form Submitted');
+                console.log('Email', email);
+                console.log('Password', password);
+            }}>
+                <div>
+                    <input
+                        value={email}
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        type="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit">Register</button>
+            </form>
+        </>
+    );
 };
