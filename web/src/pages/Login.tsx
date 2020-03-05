906ec081f7c151d597a9cf43afb4d5c5def7db16
Randal Vance Cunanan <randal@randalvance.net>
2020-03-05 22:38:44 +0800

    Add login form

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { useLoginMutation } from '../generated/graphql';
 
-interface Props {
+export const Login: React.FC<RouteComponentProps> = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [login] = useLoginMutation()
 
-}
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={async (e) => {
+                e.preventDefault();
+                const response = await login({
+                    variables: {
+                        email,
+                        password
+                    }
+                });
+                console.log(response.data?.login.accessToken);
+            }}>
+                <div>
+                    <input
+                        value={email}
+                        placeholder="Email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
 
-export const Login: React.FC<Props> = () => {
-    return <div>Login Page</div>;
+                <div>
+                    <input
+                        type="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+        </>
+    );
 };
