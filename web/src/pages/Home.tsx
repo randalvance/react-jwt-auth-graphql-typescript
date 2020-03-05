ab65fad7ff83f47533c319731aa3fa04a75ea28d
Randal Vance Cunanan <randal@randalvance.net>
2020-03-05 22:28:24 +0800

    Show users in home page

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useUsersQuery } from '../generated/graphql';
 
 interface Props {
 
 }
 
 export const Home: React.FC<Props> = () => {
+    const { data } = useUsersQuery({ fetchPolicy: 'network-only' });
+
+    if (!data) {
+        return <div>Loading...</div>
+    }
     return (
         <>
-            <div>Home</div>
+            <div>Users:</div>
+            <ul>
+                {data.users.map(user => (<li key={user.id}>{user.email} - {user.id}</li>))}
+            </ul>
         </>
     );
 };
