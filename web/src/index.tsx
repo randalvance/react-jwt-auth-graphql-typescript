e01f0f98e2a29c00b84e44a478b2eb7cc997e2de
Randal Vance Cunanan <randal@randalvance.net>
2020-03-06 21:04:29 +0800

    Include credentials

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,7 +5,8 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { Routes } from './Routes';
 
 const client = new ApolloClient({
-    uri: 'http://localhost:5000/graphql'
+    uri: 'http://localhost:5000/graphql',
+    credentials: 'include',
 });
 
 ReactDOM.render(
